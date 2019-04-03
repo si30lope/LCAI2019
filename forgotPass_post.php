351542a15b140c765d095c94efcaaf6a8be1160b
Sara <si30lope@siena.edu>
2019-04-03 13:53:19 -0400

    updated php stuff

--- forgotPass_post.php
+++ forgotPass_post.php
@@ -1,50 +1,51 @@
-
 <? 
 require_once("functions.php");
 session_start();
 if ($_SESSION['authenticated'] != true) {
   die("Access denied");	
 }
-print_html_header2("New Post");
+print_html_header2("Forgot Password");
 // Assume no submission error
 $error = false;
 // More compact numeric array for storing question data
 $q = array();
 // For keeping track of which radio button was checked
 $a = array();
-//  Only process if $_POST is not empty and the Post button was pressed 
-if (empty($_POST) == false && $_POST['action'] == "Post") {
+//  Only process if $_POST is not empty and the Insert button was pressed 
+if (empty($_POST) == false && $_POST['action'] == "Insert") {
 	// Put the data in a more compact numeric array
-	$q[0] = $_POST['postID'];
-    $q[1] = $_POST['description'];
-	$q[2] = $_POST['body'];
-
+	$q[0] = $_POST['Email'];
+	
 	// Keep track of which radio button should stay checked if there is an error
-	//$a[$_POST['answer']] = "checked";
+	$a[$_POST['answer']] = "checked";
 	
 	// Check for blank input
-	foreach ($q as $value) {
+    foreach ($q as $value) {
 		if ($value == "") {
 			$error = true;
-			break;
+            $mysqli = db_connect();				
+            $sql = "SELECT email
+            FROM users
+            WHERE email == .$q[value]";
+            $mysqli->query($sql);
+            echo '<a href="forgotPass_post.php">Forgot Password</a>';
+
+            print_question_table($mysqli);
+
+            $mysqli->close();
+			
 		}
 	}
 	
 	// Make sure all the data is present
 	if ($error == false) {	
-		$mysqli = db_connect();				
-		$sql = "INSERT INTO newPost (postID, description, body) VALUES ('".$q[0]."','".$q[1]."','".$q[2]."')";
-		$mysqli->query($sql);
-		echo '<a href="newPost_post.php">Write Another Post</a>';
-		
-		print_post_table($mysqli);
-		
-		$mysqli->close();
+		echo '<a href="forgotPass_post.php">No Account on File</a>';
+        
 	}
 }
 // If the post is not set or if there is an error
 if (empty($_POST) == true || $error == true) {
-	print_post_form($error, $q, $c);
+	echo '<a> Email has been sent</a>';
 }
 print_html_footer2();
 ?>
--- functions.php
+++ functions.php
@@ -1,38 +1,344 @@
-<?php
-define('DB_NAME', 'perm_chat');
-define('DB_USERNAME', 'perm_chat');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'cs.siena.edu');
-define('DB_CHARSET', 'utf8');
 
-
-
-function connectDB($user, $pass, $db) {
-    // Create connection
-        //$link = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
-        $link = mysql_connect($host, $user, $pass);
-
-    // Check connection
-    if (!$link) {
-        die("Connection failed: " . mysql_error());
-    } 
-
-    //$db_selected = mysql_select_db(DB_NAME, $link);
-    $db_selected = mysql_select_db($db, $link);
-
-    if(!$db_selected){
-        //die("Cannot use " . DB_NAME . ': ' . mysql_error());
-        die("Cannot use " . $db . ': ' . mysql_error());
-    }
-
-    echo "Connected successfully";
+<?
+// Connects to database
+function db_connect() {
+	return new mysqli("perm_chat", "perm_chat", "s2019LCAI", "cs.siena.edu");	
+}
+/* ------------------------- Document Structure Functions ---------------------------- */
+// Print html header with bootstrap css and page title
+function print_html_header2($title) {
+	if ($title != "Login")
+		$menu = make_menu_bs($title);
+	echo '
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <title>'.$title.'</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="container">
+			'.$menu.'
+			<div class="content">
+	';
+}
+// Print html footer with bootstrap js
+function print_html_footer2() {
+	echo '
+			</div> <!-- /content -->			 
+		</div> <!-- /container -->			 
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
+	';
+}
+function print_login_bs($error) {
+	if ($error) {
+		$message = '<div class="alert alert-danger">Incorrect username or password</div>';
+	}
+	echo '
+			<form class="form-signin" method="post" action="index.php">
+		    <h2 class="form-signin-heading">Login</h2>
+		    
+		    <label for="username" class="sr-only">Email address</label>
+		    
+		    <input name="username" type="text" id="username" class="form-control" placeholder="Username" required autofocus>
+		    
+		    
+		    <label for="password" class="sr-only">Password</label>
+		    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+		    '.$message.'
+		    <input class="btn btn-md btn-primary" type="submit" name="action" value="Login">
+		    <a href="insert_user.php">Register New Account</a>
+		  </form>
+		 
+	';
+}
+/* ------------------------- Main Menu Functions ---------------------------- */
+// Get main menu items
+function get_menu_items($title) {
+	$items['home.php'] = "Home";
+	$items['trivia.php'] = "Play Trivia";
+	$items['leader.php'] = "View Leader Board";
+	$items['rank_question.php'] = "Rank Questions";
+	$items['insert_question.php'] = "Insert Question";
+	
+	if ($_SESSION['usertype'] == "admin") {	
+		$items['insert_user_admin.php'] = "Insert User";
+		$items['delete_question.php'] = "Delete Question";
+		$items['delete_user.php'] = "Delete Users";
+		
+	}		
+	$items['logout.php'] = "Logout";
+		
+	foreach ($items as $key=>$value) {
+		$active = '';
+		if ($value==$title) $active = "active";
+		$menu_items .= '
+		  <li class="nav-item">
+		    <a class="nav-link '.$active.'" href="'.$key.'">'.$value.'</a>
+		  </li>
+		';
+	}
+	return $menu_items;
+}
+// Make main menu with basic Bootstrap classes
+function make_menu($title) {;	
+	$menu = '<ul class="nav nav-pills">';
+  $menu .= get_menu_items($title);
+	$menu .= '</ul>';
+	return $menu;
+}
+// Make main menu with responsive Bootstrap classes
+function make_menu_bs($title) {
+	$menu = '
+	<nav class="navbar navbar-inverse bg-inverse navbar navbar-toggleable-md navbar-light bg-faded">
+  	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" 
+  	        data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    	<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="home.php">Trivia</a>
+		<div class="collapse navbar-collapse" id="navbarNav">
+    	<ul class="navbar-nav">';
+	$menu .= get_menu_items($title);
+	$menu .= '
+			</ul>
+		</div>
+	</nav>
+	</nav>';
+	return $menu;
+}
+// Print html header with bootstrap css and page title
+function print_html_header($title) {
+	$items['home.php'] = "Home";
+	$items['insert_question.php'] = "Insert Question";	
+	$items['trivia.php'] = "Play Trivia";	
+	$items['leader.php'] = "View Leader Board";	
+	$items['showUserTable.php'] = "View Users";
+	
+	if($_SESSION['usertype'] == 'admin'){
+		$items['delete_question.php'] = "Delete Question";	
+	}
+	$items['logout.php'] = "Logout";	
+	if($title == "Login"){
+		$menu = "";
+	}
+	else{
+		$menu = '<ul class="nav nav-pills">';
+		foreach ($items as $key=>$value) {
+			$active = "";
+			if ($value==$title) $active = "active";
+			$menu .= '
+			<li class="nav-item">
+				<a class="nav-link '.$active.'" href="'.$key.'">'.$value.'</a>
+			</li>';
+			}
+		
+		}
+		$menu .= '</ul>';
+	
+	
+	echo '
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	  <title>'.$title.'</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="container">
+			'.$menu.'
+			<h2>'.$title.'</h2>
+	';
+}
+// Print html footer with bootstrap js
+function print_html_footer() {
+	echo '
+		</div> <!-- /container -->	
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+	</body>
+</html>
+	';
+}
+// Print the question table
+function print_post_table($mysqli) {
+	// First, print the columns, i.e, field names
+	$result = $mysqli->query("SHOW COLUMNS FROM newPost");
+	echo '<table class="table table-striped">';
+	echo '<thead class="thead-inverse">';
+	echo '<tr>';
+	while ($row = $result->fetch_row()) {
+		echo '<th>'.$row[0]."</th>";
+	}
+	echo '</tr>';
+	echo '<thead>';
+	$result->close();
+	
+	// Second, print all the rows
+	$result = $mysqli->query("SELECT * FROM newPost ORDER BY id DESC");
+	echo '<tbody>';
+	while ($row = $result->fetch_row()) {
+		echo '<tr>';
+		foreach ($row as $value) {
+			echo '<td>'.$value.'</td>';
+		}
+		echo '</tr>';
+	}
+	echo '</tbody>';
+	echo '</table>';
+	$result->close();
+}
+// Print the login form
+function print_login_form($error) {
+	echo '
+	<h2> Welcome to Trivia!</h2><br>
+ 
+    <form method="post" action="index.php">
+    <label>
+    	<input name="username" type="text" placeholder="Username">
+    </label>
+	<br>
+    <label> 
+    	<input name="password" type="password" placeholder="Password">
+    </label>
+    <p>
+        <a href="insert_user.php">Register New Account</a>
+    </p>
+    <input type="submit" name="action" value="Login"> 
+    '.$error.'
+  </form>
+  </div>
+  ';
+}
+// Print the insert question form 
+function print_post_form($error, $q, $a) {
+	if ($error) 
+		$message = "All fields must be filled out";
+	echo '		
+  <form method="post" action="insert_question.php">
+  	
+		<label>Question<br>
+		<textarea name="question" rows="3">'.$q[0].'</textarea>
+		</label><br>
+		
+		<label>Choice 1
+		<br>
+		<input type="text" name="choice1" size="50" value="'.$q[1].'">
+		</label>
+		<input type="radio" name="answer" value="1" '.$a[1].'>
+		<br>
+		
+		<label>Choice 2
+		<br>
+		<input type="text" name="choice2" size="50" value="'.$q[2].'">
+		</label>
+		<input type="radio" name="answer" value="2" '.$a[2].'>
+		<br>
+		
+		<label>Choice 3
+		<br>
+		<input type="text" name="choice3" size="50" value="'.$q[3].'">
+		</label>
+		<input type="radio" name="answer" value="3" '.$a[3].'>
+		<br>
+		
+		<label>Choice 4
+		<br>
+		<input type="text" name="choice4" size="50" value="'.$q[4].'">
+		</label>
+		<input type="radio" name="answer" value="4" '.$a[4].'>
+		<br>
+					
+		<input type="submit" name="action" value="Insert">
+		'.$message.'
+	</form>
+	';
+}
+function print_post($q, $c1, $c2, $c3, $c4)  {
+	echo '		
+  <form method="post" action="get_question.php">
+  	
+		<label>Question<br>
+		<h2 name="question" rows="3">'.$q.'</h2>
+		</label><br>
+		
+		<label>
+		'.$c1.' 
+		<input type="radio" name="answer" value="1">
+		</label><br>
+          
+        <label>
+		'.$c2.'
+		<input type="radio" name="answer" value="2">
+		</label><br>
+          
+        <label>
+		'.$c3.' 
+		<input type="radio" name="answer" value="3">
+		</label><br>
+          
+        <label>
+		'.$c4.'
+		<input type="radio" name="answer" value="4">
+		</label><br>
+        
+        <input type="submit" value="Next" name="action">
+		'.$message.'
+	</form>
+	';
+	
 }
 
-function f_clean($array) {
-    return array_map('mysql_real_escape_string', $array);
+function print_only_question($q)  {
+	echo '		
+  <form method="post" action="get_question.php">
+  	
+		<label><br>
+		<h2 name="question" rows="3">'.$q.'</h2>
+        <input type="radio" name="answer" value="1">
+		</label><br>
+	</form>
+	';
+	}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
 ?>
-
-
-
